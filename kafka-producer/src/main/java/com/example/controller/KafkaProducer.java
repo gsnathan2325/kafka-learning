fa3cc7d6fcef9482ea791d2c066d79617c64b2aa
gsnathan2325 <gsnathan77@yahoo.com>
2026-06-23 17:21:51 -0400

    Test the kafka producer

---
+++ kafka-producer/src/main/java/com/example/controller/KafkaProducer.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+}
