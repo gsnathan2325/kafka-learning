c91a3e5655481bbaa5f117e222cecad6abf732f3
gsnathan2325 <gsnathan77@yahoo.com>
2026-06-24 05:44:44 -0400

    Consumer deserializer and perosn object adeed

--- kafka-consumer/src/main/java/com/example/consumer/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -3,11 +3,13 @@ package com.example.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.example.model.Person;
+
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics ="my-new-topic", groupId="my-consumer-group")
-    public void listen(String message){
-        System.out.println("Incoming message" +message);
+    @KafkaListener(topics ="my-new-topic-1", groupId="my-consumer-group")
+    public void listen(Person person){
+        System.out.println("Incoming message. " +person);
     }
 }
--- kafka-producer/src/main/java/com/example/controller/KafkaProducer.java
+++ kafka-producer/src/main/java/com/example/controller/KafkaProducer.java
@@ -1,28 +1,31 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Person;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequestMapping("/api")
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Person> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, Person> kafkaTemplate) {
         System.out.println("TEST");
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
-    public String sendMessage(@RequestParam String message) {
-
-        kafkaTemplate.send("my-new-topic-1", message);
-        return "Message sent :" + message;
+    public String sendMessage(@RequestBody Person person) {
+        //Person person = new Person("Saminathan", "Govindan", 45);
+        kafkaTemplate.send("my-new-topic-1", person);
+        return "Message sent :" + person.toString();
     }
 
 }
